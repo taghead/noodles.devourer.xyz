50e5eb2c23946282087c909dfc8953d381af0844
Andrew Chander <13403032+taghead@users.noreply.github.com>
2023-04-14 23:44:03 +1000

    Fixed .vscode folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,10 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className={styles.main}>
-      <h1 className="underline">
-      Hello world!
-    </h1>
+        <h1 className="underline">Hello world!</h1>
       </main>
     </>
-  )
+  );
 }
